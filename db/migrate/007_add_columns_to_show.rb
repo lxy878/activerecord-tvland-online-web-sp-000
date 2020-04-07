ae81096f0e7994de783458041cc29e56de7629bb
Xiaoyi Liu <lxy878@gmail.com>
2020-04-07 10:24:32 +0000

    Automatically backed up by Learn

--- db/migrate/007_add_columns_to_show.rb
+++ db/migrate/007_add_columns_to_show.rb
@@ -2,7 +2,7 @@ class AddColumnsToShow < ActiveRecord::Migration[5.1]
 
   def change
     add_column :shows, :day, :string
-    add_column :shows, :genres, :string
+    add_column :shows, :genre, :string
     add_column :shows, :season, :string
   end
 end
