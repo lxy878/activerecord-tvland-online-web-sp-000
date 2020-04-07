5379a7f3678962929f1bb25c8573c9618d27a07a
Xiaoyi Liu <lxy878@gmail.com>
2020-04-07 09:38:14 +0000

    Automatically backed up by Learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,8 +1,8 @@
 class CreateActors < ActiveRecord::Migration[5.1]
   def change
     create_table :actors do |t|
-      :first_name
-      :last_name
+      t.string :first_name
+      t.string :last_name
     end
   end
 end
